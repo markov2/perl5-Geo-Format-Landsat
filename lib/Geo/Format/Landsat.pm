223dde8845128b3b5f7ac4537fe0702610257a64
Mark Overmeer <mark@overmeer.net>
2018-01-23 17:23:36 +0100

    convert to GIT and GitHUB

---
+++ lib/Geo/Format/Landsat.pm
@@ -0,0 +1,34 @@
+# This code is part of distribution Geo-Format-Landsat.  Meta-POD processed
+# with OODoc into POD and HTML manual-pages.  See README.md
+# Copyright Mark Overmeer.  Licensed under the same terms as Perl itself.
+
+package Geo::Format::Landsat;
+
+# Intentionally empty
+
+=chapter NAME
+
+Geo::Format::Landsat - read landsat meta data files
+
+=chapter SYNOPSIS
+
+ # See Geo::Format::Landsat::MTL
+
+=chapter DESCRIPTION
+
+This package is a place-holder for extension.  "New" CPAN rules
+require each distribution to contain a package with the same name
+as the distribution has (replace '-' by '::')
+
+The distribution only contains an implementation to parse MTL files,
+but there may be more in the futuren (sponsor me!)
+
+=over 4
+
+=item M<Geo::Format::Landsat::MTL>
+
+=back
+
+=cut
+
+1;
